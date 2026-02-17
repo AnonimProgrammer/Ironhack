020b398e25726aa044c09a817a896e8e61a74a29
AnonimProgrammer <zephyr11ff@gmail.com>
2026-02-17 17:04:22 +0400

    First day commit. Foundational concepts covered

---
+++ src/main/java/com/ironhack/day1/DataTypes.java
@@ -0,0 +1,4 @@
+package com.ironhack.day1;
+
+public class DataTypes {
+}
---
+++ src/main/java/com/ironhack/day1/FoundationalDataTypes.java
@@ -0,0 +1,15 @@
+package com.ironhack.day1;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = "";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(i).append(", ");
+        }
+        result = sb.toString();
+
+        System.out.println("Result: " + result);
+    }
+}
