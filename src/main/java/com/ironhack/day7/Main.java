06d77dc5b324b0d36b1b05d51161a9f5f49054e8
AnonimProgrammer <zephyr11ff@gmail.com>
2026-02-24 14:55:30 +0400

    JSON and Maven covered.

---
+++ src/main/java/com/ironhack/day7/Main.java
@@ -0,0 +1,8 @@
+package com.ironhack.day7;
+
+public class Main {
+    public static void main(String[] args) {
+        GsonSerializationDemo.run();
+        GsonDeserializationDemo.run();
+    }
+}
